2d79d8170382cfbfc900466dc57a637e1c436743
kusalpranama <kusalpranama2003@gmail.com>
2024-12-04 11:55:05 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ItemComponent } from './pages/item/item.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:AppComponent
+    },
+    {
+        path:"Home",
+        component:HomeComponent
+    },
+    {
+        path:"Item",
+        component:ItemComponent
+    },
+    {
+        path:"Contact",
+        component:ContactComponent
+    }
+];
